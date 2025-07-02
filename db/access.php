daaa984c52532e66936349967be277900f8ee529
Ken Farrimond <KenFarr@users.noreply.github.com>
2025-07-02 22:35:58 +0100

    Update access.php

--- db/access.php
+++ db/access.php
@@ -1,12 +1,22 @@
 <?php
 
 $capabilities = [
+
     'mod/readepub:view' => [
         'captype' => 'read',
         'contextlevel' => CONTEXT_MODULE,
         'archetypes' => [
             'student' => CAP_ALLOW,
             'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ]
+    ],
+
+    'mod/readepub:addinstance' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => [
             'editingteacher' => CAP_ALLOW,
             'manager' => CAP_ALLOW
         ]
