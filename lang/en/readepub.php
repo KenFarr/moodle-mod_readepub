2a3b1b4a88b0ee89051d136fbd5f257a5b161f85
Ken Farrimond <KenFarr@users.noreply.github.com>
2025-07-02 22:36:27 +0100

    Update readepub.php

--- lang/en/readepub.php
+++ lang/en/readepub.php
@@ -6,3 +6,4 @@ $string['modulenameplural'] = 'Read EPUBs';
 $string['bookname'] = 'Book folder name';
 $string['bookname_help'] = 'Enter the folder name under bibi-bookshelf where the EPUB is stored.';
 $string['noreadepubs'] = 'There are no EPUB reading activities in this course.';
+$string['readepub:addinstance'] = 'Add a new EPUB reading activity';
