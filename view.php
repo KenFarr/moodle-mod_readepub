1f360a3721d412fb389d8937f61baa6605572678
Ken Farrimond <KenFarr@users.noreply.github.com>
2025-07-02 22:40:14 +0100

    Update view.php

--- view.php
+++ view.php
@@ -3,28 +3,34 @@
 require(__DIR__.'/../../config.php');
 require_once(__DIR__.'/lib.php');
 
-$id = required_param('id', PARAM_INT);
+$id = required_param('id', PARAM_INT); // Course module ID
 $cm = get_coursemodule_from_id('readepub', $id, 0, false, MUST_EXIST);
 $context = context_module::instance($cm->id);
-
 require_course_login($cm->course, true, $cm);
 
 $readepub = $DB->get_record('readepub', ['id' => $cm->instance], '*', MUST_EXIST);
+
+// Build the URL to launch
+$bookurl = new moodle_url('/mod/readepub/bibi/index.php', ['book' => $readepub->bookname]);
 
 $PAGE->set_url('/mod/readepub/view.php', ['id' => $cm->id]);
 $PAGE->set_title($readepub->name);
 $PAGE->set_heading($COURSE->fullname);
 
+// Output Moodle page with JS redirect
 echo $OUTPUT->header();
 echo $OUTPUT->heading(format_string($readepub->name));
-
 echo format_module_intro('readepub', $readepub, $cm->id);
 
-// Embed the Bibi reader
-$bookurl = new moodle_url('/mod/readepub/bibi/index.php', ['book' => $readepub->bookname]);
-echo html_writer::tag('iframe', '', [
-    'src' => $bookurl->out(),
-    'style' => 'width:100%;height:600px;border:0;'
+echo html_writer::tag('p', get_string('launchingbook', 'readepub'));
+echo html_writer::link($bookurl, get_string('clickherelaunch', 'readepub'), [
+    'target' => '_blank',
+    'class' => 'btn btn-primary'
 ]);
+
+// JS auto-launch
+echo html_writer::script("
+    window.open('{$bookurl->out()}', '_blank');
+");
 
 echo $OUTPUT->footer();
