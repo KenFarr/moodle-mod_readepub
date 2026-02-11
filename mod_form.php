5c84ec991bc14a9288414333adbccdf9bbb53d4a
Ken Farrimond <KenFarr@users.noreply.github.com>
2026-02-11 13:05:13 +0000

    Refactor mod_form.php for improved structure

--- mod_form.php
+++ mod_form.php
@@ -1,9 +1,12 @@
 <?php
 
-require_once($CFG->dirroot.'/course/moodleform_mod.php');
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/course/moodleform_mod.php');
 
 class mod_readepub_mod_form extends moodleform_mod {
-    function definition() {
+
+    public function definition() {
         $mform = $this->_form;
 
         // Activity name
@@ -15,11 +18,21 @@ class mod_readepub_mod_form extends moodleform_mod {
         $this->standard_intro_elements();
 
         // Book folder name
-        $mform->addElement('text', 'bookname', get_string('bookname', 'readepub'));
+        $mform->addElement('text', 'bookname', get_string('bookname', 'mod_readepub'));
         $mform->setType('bookname', PARAM_ALPHANUMEXT);
         $mform->addRule('bookname', null, 'required', null, 'client');
 
         $this->standard_coursemodule_elements();
         $this->add_action_buttons();
+    }
+
+    public function validation($data, $files) {
+        $errors = parent::validation($data, $files);
+
+        if (empty($data['bookname'])) {
+            $errors['bookname'] = get_string('required');
+        }
+
+        return $errors;
     }
 }
