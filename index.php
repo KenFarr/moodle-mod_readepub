42bfc8e25da03ba621309870f776713093736681
Ken Farrimond <KenFarr@users.noreply.github.com>
2026-02-11 13:14:52 +0000

    Refactor readepub module for improved context handling

--- index.php
+++ index.php
@@ -6,25 +6,34 @@ require_once($CFG->dirroot . '/mod/readepub/lib.php');
 $id = required_param('id', PARAM_INT); // Course ID
 
 $course = get_course($id);
-require_course_login($course);
+require_login($course);
+
+$context = context_course::instance($course->id);
 
 $PAGE->set_url('/mod/readepub/index.php', ['id' => $id]);
-$PAGE->set_title(get_string('modulenameplural', 'readepub'));
-$PAGE->set_heading($course->fullname);
+$PAGE->set_context($context);
+$PAGE->set_title(get_string('modulenameplural', 'mod_readepub'));
+$PAGE->set_heading(format_string($course->fullname));
 
 echo $OUTPUT->header();
-echo $OUTPUT->heading(get_string('modulenameplural', 'readepub'));
+echo $OUTPUT->heading(get_string('modulenameplural', 'mod_readepub'));
 
 // Get all instances of the activity in this course
 if (!$readepubs = get_all_instances_in_course('readepub', $course)) {
-    echo $OUTPUT->notification(get_string('noreadepubs', 'readepub'), 'notifymessage');
+    echo $OUTPUT->notification(
+        get_string('noreadepubs', 'mod_readepub'),
+        \core\output\notification::NOTIFY_INFO
+    );
     echo $OUTPUT->footer();
     exit;
 }
 
 // Prepare the table
 $table = new html_table();
-$table->head  = [get_string('name'), get_string('bookname', 'readepub')];
+$table->head  = [
+    get_string('name'),
+    get_string('bookname', 'mod_readepub')
+];
 $table->align = ['left', 'left'];
 
 foreach ($readepubs as $readepub) {
@@ -33,7 +42,10 @@ foreach ($readepubs as $readepub) {
         format_string($readepub->name)
     );
 
-    $table->data[] = [$link, format_string($readepub->bookname)];
+    $table->data[] = [
+        $link,
+        format_string($readepub->bookname)
+    ];
 }
 
 echo html_writer::table($table);
