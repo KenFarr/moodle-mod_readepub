383c9211ee7502e7ea561f9b71f4805f0d302a1c
Ken Farrimond <KenFarr@users.noreply.github.com>
2026-02-11 13:12:29 +0000

    Add privacy metadata string to readepub.php

--- lang/en/readepub.php
+++ lang/en/readepub.php
@@ -4,12 +4,19 @@ $string['pluginname'] = 'Read EPUB';
 $string['pluginadministration'] = 'Read EPUB administration';
 $string['modulename'] = 'Read EPUB';
 $string['modulenameplural'] = 'Read EPUBs';
+
 $string['bookname'] = 'Book folder name';
 $string['bookname_help'] = 'Enter the folder name under bibi-bookshelf where the EPUB is stored.';
+
 $string['readepub:addinstance'] = 'Add a new EPUB reading activity';
 $string['readepub:view'] = 'View EPUB activity';
+
 $string['invalidbook'] = 'Invalid book specified.';
 $string['noreadepubs'] = 'There are no EPUB reading activities in this course.';
+
 $string['launchingbook'] = 'Launching the book in a new tab...';
 $string['clickherelaunch'] = 'Click here if the book doesn\'t open automatically';
+
+$string['privacy:metadata'] = 'The Read EPUB activity does not store any personal data.';
+
 $string['name'] = 'Name';
