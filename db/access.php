6bf74256e52bc6f28d3736b19d81a94cc4f48e8e
Ken Farrimond <KenFarr@users.noreply.github.com>
2026-02-11 13:10:09 +0000

    Fix capability definitions for manager role

--- db/access.php
+++ db/access.php
@@ -1,24 +1,29 @@
 <?php
 
+defined('MOODLE_INTERNAL') || die();
+
 $capabilities = [
 
     'mod/readepub:view' => [
+        'riskbitmask' => RISK_XSS,
         'captype' => 'read',
         'contextlevel' => CONTEXT_MODULE,
         'archetypes' => [
             'student' => CAP_ALLOW,
             'teacher' => CAP_ALLOW,
             'editingteacher' => CAP_ALLOW,
-            'manager' => CAP_ALLOW
-        ]
+            'manager' => CAP_ALLOW,
+        ],
     ],
 
     'mod/readepub:addinstance' => [
+        'riskbitmask' => RISK_XSS,
         'captype' => 'write',
         'contextlevel' => CONTEXT_COURSE,
         'archetypes' => [
             'editingteacher' => CAP_ALLOW,
-            'manager' => CAP_ALLOW
-        ]
-    ]
+            'manager' => CAP_ALLOW,
+        ],
+        'clonepermissionsfrom' => 'moodle/course:manageactivities',
+    ],
 ];
