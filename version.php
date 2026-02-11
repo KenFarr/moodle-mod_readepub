fc5a35f95428a7aa5d7c6e5f2e2dbc18882eebc5
Ken Farrimond <KenFarr@users.noreply.github.com>
2026-02-11 12:46:39 +0000

    Update version.php

--- version.php
+++ version.php
@@ -4,7 +4,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_readepub';
 $plugin->version = 2025070301;
-$plugin->requires = 2022041900; // Moodle 4.0 or later
+$plugin->requires = 2022041900; // Moodle 4.5 or later
 $plugin->cron = 0;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = '0.1';
